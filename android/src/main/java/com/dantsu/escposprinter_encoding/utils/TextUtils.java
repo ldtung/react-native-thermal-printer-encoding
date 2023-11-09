bb6ef4125039a9b9559c2212a1bd33d4730042ba
Tung Le <ldtung.it@gmail.com>
2023-11-09 08:55:40 +0700

    Support TCVN2

--- android/src/main/java/com/dantsu/escposprinter_encoding/utils/TextUtils.java
+++ android/src/main/java/com/dantsu/escposprinter_encoding/utils/TextUtils.java
@@ -1,6 +1,10 @@
 package com.dantsu.escposprinter_encoding.utils;
 
+import com.tinyhack.vncharset.TCVN3;
+import com.tinyhack.vncharset.VNCharsetProvider;
+
 import java.io.UnsupportedEncodingException;
+import java.nio.charset.Charset;
 
 public class TextUtils {
 
@@ -13,7 +17,11 @@ public class TextUtils {
       return null;
     }
     if ("TCVN-3-1".equalsIgnoreCase(charset)) {
-      return strText.getBytes(charset);
+      Charset charsetObj = new VNCharsetProvider().charsetForName("TCVN-3-1");
+      return strText.getBytes(charsetObj);
+    } else if ("TCVN-3-2".equalsIgnoreCase(charset)) {
+      Charset charsetObj = new VNCharsetProvider().charsetForName("TCVN-3-2");
+      return strText.getBytes(charsetObj);
     }
     return strText.getBytes(charset);
   }
